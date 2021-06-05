8c3c4f30db215581d58c706bcedbd766b82d9c3f
堤宏太 <kota-t@weseek.co.jp>
2021-06-05 14:37:18 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
-function App() {
-  return (
-    <div>
-      <p>
-        Hello world!!
-      </p>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <h1>
+//         Hello world!!
+//     </h1>
+//   );
+// }
 
+// import React, { Component } from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}}></input>
+      </>
+    )
+  }
+}
 export default App;
